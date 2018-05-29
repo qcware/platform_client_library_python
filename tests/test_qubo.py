5dcc2f47fecab9312ca28c0fe563a2faaddcfee1
Vic Putz <vic.putz@qcware.com>
2018-05-29 16:36:09 -0500

    fixed minor pep8 errors in test, tried adding pyenv to circle.yml as python3 not available to tests

--- tests/test_qubo.py
+++ tests/test_qubo.py
@@ -4,6 +4,7 @@ import numpy as np
 
 Aqua_key = os.environ['AQUA_TEST_KEY']
 
+
 def test_qubo():
     Q = {(0, 0): 1, (0, 1): 1, (1, 1): 1, (1, 2): 1, (2, 2): -1}
     solver = 'dwave_software'
@@ -20,5 +21,3 @@ def test_qubo():
 
 if __name__ == "__main__":
     test_qubo()
-
-
