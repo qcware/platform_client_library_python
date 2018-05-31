d981554f64f947b37ad4fd2f1f849fef975620f7
Vic Putz <vic.putz@qcware.com>
2018-05-31 12:11:20 -0500

    added aqua_host as env var for test context

--- tests/test_qubo.py
+++ tests/test_qubo.py
@@ -2,17 +2,18 @@ import qcware
 import os
 import numpy as np
 
-Aqua_key = os.environ['AQUA_TEST_KEY']
+AQUA_KEY = os.environ['AQUA_TEST_KEY']
+AQUA_HOST = os.environ['AQUA_HOST']
 
 
 def test_qubo():
     Q = {(0, 0): 1, (0, 1): 1, (1, 1): 1, (1, 2): 1, (2, 2): -1}
     solver = 'dwave_software'
     result = qcware.optimization.solve_binary(
-        Aqua_key,
+        AQUA_KEY,
         Q,
         solver=solver,
-        host="http://platform.qcware.com")
+        host=AQUA_HOST)
     assert(np.equal(
         result['solution'],
         np.array([0, 0, 1])).all())
