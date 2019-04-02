f74f6d3a6d569a6e03ced854b55f252371c95418
gcdstillhungry <tqcuistep@gmail.com>
2019-04-02 04:48:25 -0500

    Update version numbering

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import setuptools
 
 
 setup(
-  name='qcware',
+  name='qcwaretest',
   packages=['qcware'],
-  version='0.2.16',
+  version='0.2.19',
   description='Functions for easily interfacing with the QC Ware Platform from Python',
   author='QC Ware Corp.',
   author_email='info@qcware.com',
   url='https://github.com/qcware/platform_client_library_python',
-  download_url='https://github.com/qcware/platform_client_library_python/tarball/0.2.15',
+  download_url='https://github.com/qcware/platform_client_library_python/tarball/0.2.19',
   keywords=['quantum', 'computing', 'cloud', 'API'],
   classifiers=[],
   install_requires=[
