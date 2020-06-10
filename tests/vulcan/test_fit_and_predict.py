484d7e8fb1e4c552090758c11f9ef673efebe7af
Vic Putz <vic.putz@qcware.com>
2020-06-10 02:30:50 +0000

    automated push from 3add1742a2576874442909ebe5f5a04be8f3b45c

---
+++ tests/vulcan/test_fit_and_predict.py
@@ -0,0 +1,13 @@
+import qcware
+import numpy as np
+
+
+def test_fit_and_predict():
+    X = np.array([[-1, -2, 2, -1], [-1, -1, 2, 0], [2, 1, -2, -1],
+                  [1, 2, 0, -1]])
+    y = np.array([0, 0, 1, 1])
+    result = qcware.qml.fit_and_predict(X=X,
+                                        y=y,
+                                        model="QNearestCentroid",
+                                        backend="vulcan/simulator")
+    assert (result == [0, 0, 1, 1]).all()
