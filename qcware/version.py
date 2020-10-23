0994081ae1a35728d8ecd540df40872d67f593a2
Vic Putz <vic.putz@qcware.com>
2020-10-23 20:58:04 +0000

    automated push from e1607c849ee7e097275cf198b817781ff6295813

--- qcware/version.py
+++
@@ -1,9 +0,0 @@
-from distutils.version import StrictVersion
-
-
-def parameter_api_version():
-    return "2.2"
-
-
-def version_is_greater(version_str):
-    return StrictVersion(version_str) > StrictVersion(parameter_api_version())
