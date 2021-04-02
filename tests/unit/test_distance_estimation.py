5f0d2225b7b20eef9b95e73ca55589e96d33d128
Vic Putz <vic.putz@qcware.com>
2021-04-02 21:55:50 +0000

    automated push from 8998ae9d91061ccbd6ba83a12d27a24afb3c9a5c

--- tests/unit/test_distance_estimation.py
+++ tests/unit/test_distance_estimation.py
@@ -1,15 +1,22 @@
 from qcware.qutils import distance_estimation
 import numpy as np
-from hypothesis.extra.numpy import arrays
+import pytest
+import itertools
+
+backends = (('qcware/cpu_simulator', None), ('awsbraket/sv1', 100),
+            ('ibm/simulator', 100), ('qcware/gpu_simulator', None))
 
 
-def test_distance_estimation():
+@pytest.mark.parametrize('backend, num_measurements',
+                         backends)
+def test_distance_estimation(backend, num_measurements):
     # purely smoke test
     x = np.random.rand(4)
     y = np.random.rand(4)
     x = x / np.linalg.norm(x)
     y = y / np.linalg.norm(y)
 
-    result = distance_estimation(x, y)
+    result = distance_estimation(x, y, backend=backend, num_measurements=num_measurements)
     distance = np.linalg.norm(x - y)**2
-    assert np.allclose(result, distance)
+    # huge atol since this is mostly a smoke test
+    assert np.allclose(result, distance, atol=2)
--- tests/unit/test_qdot.py
+++ tests/unit/test_qdot.py
@@ -1,7 +1,7 @@
 from qcware.qutils import qdot
 import numpy as np
 import pytest
-
+import itertools
 # the tricky thing here for serialization is to make
 # sure that the types come out right.  For dot,
 # we have
@@ -12,21 +12,29 @@ import pytest
 # [m]->[mxn]->[n]
 # otherwise throw an exception
 
-
 # I really wanted to use hypothesis here, but the fact is it
 # takes too long to send it over the wire, so we'll do case studies
 # for each
-@pytest.mark.parametrize(('x,y'), [
-    (np.array([5]), np.array([5])),
-    (np.array([[5,4,3],[2,1,0]]), np.array([8,7,6])),
-])
-def test_qdot(x, y):
+backends = (('qcware/cpu_simulator', None), ('awsbraket/sv1', 100),
+            ('ibm/simulator', 100), ('qcware/gpu_simulator', None))
 
-    result = qdot(x, y)
+
+def flatten(x):
+    return list(itertools.chain.from_iterable(x))
+
+
+@pytest.mark.parametrize(
+    'x, y, backend, num_measurements',
+    (flatten(x) for x in itertools.product(((np.array([5]), np.array([5])),
+                                            (np.array([[5, 4, 3], [2, 1, 0]]),
+                                             np.array([8, 7, 6]))), backends)))
+def test_qdot(x, y, backend, num_measurements):
+    result = qdot(x, y, backend=backend, num_measurements=num_measurements)
     numpy_result = np.dot(x, y)
     if np.isscalar(numpy_result):
         assert np.isscalar(result)
     elif isinstance(numpy_result, np.ndarray):
         assert isinstance(result,
                           np.ndarray) and result.shape == numpy_result.shape
-    assert np.allclose(result, numpy_result)
+    # big tolerance here since this is more or less a smoke test for the client
+    assert np.allclose(result, numpy_result, atol=7)
